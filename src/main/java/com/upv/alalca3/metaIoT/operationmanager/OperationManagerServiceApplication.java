ab2ad6fa26217f336c9e23fe633ce62542e6f4c5
alejandroalbert998@gmail.com <alejandroalbert998@gmail.com>
2023-11-06 19:29:43 +0100

    Initial commit

---
+++ src/main/java/com/upv/alalca3/metaIoT/operationmanager/OperationManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upv.alalca3.metaIoT.operationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upv/alalca3/metaIoT/operationmanager/OperationManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upv.alalca3.metaIoT.operationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
