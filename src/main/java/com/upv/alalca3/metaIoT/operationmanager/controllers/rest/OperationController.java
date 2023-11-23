537c51bcbc3b86c9502751dc4f285f0d6d7d2e29
Alejandro Albert Casañ <amalbert@gmv.com>
2023-11-23 12:12:08 +0100

    Rest Controller

--- src/main/java/com/upv/alalca3/metaIoT/operationmanager/controllers/rest/OperationController.java
+++ src/main/java/com/upv/alalca3/metaIoT/operationmanager/controllers/rest/OperationController.java
@@ -3,17 +3,25 @@
  */
 package com.upv.alalca3.metaIoT.operationmanager.controllers.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.upv.alalca3.metaIoT.operationmanager.model.Operation;
+import com.upv.alalca3.metaIoT.operationmanager.service.IMqttService;
+
 /**
- * @author amna
  *
  */
+@RestController
 public class OperationController {
+	@Autowired
+	private IMqttService mqttService;
 
-	/**
-	 * 
-	 */
-	public OperationController() {
-		// TODO Auto-generated constructor stub
+	@PostMapping("/op/publish")
+	public ResponseEntity<Void> publishOperation(@RequestBody Operation operation) {
+		return ResponseEntity.ok().build();
 	}
-
 }
